5ae98e7420ec6508847d5098d08dbde5e8cda852
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-04 17:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function mapToNegativize(n){
+  let mapped = n.map(elem => elem*(-1))
+  return mapped
+}
+function mapToNoChange(w){
+  let print = w.map(elem => elem)
+  return print
+}
+function mapToDouble(d){
+  let doubled = d.map(elem => elem*2)
+  return doubled
+}
+function mapToSquare(s){
+  let squared = s.map(elem => elem**2)
+  return squared
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+  
+  let sa = sourceArray.reduce((accumulator, currentValue) => { accumulator+currentValue}, startingPoint) 
+  return sa
+}
